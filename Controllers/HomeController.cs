de1ee3f58a74f50eddbc65293a56bea50f7783e3
David <davidrek2008@hotmail.com>
2022-02-01 09:49:41 -0300

    corrigido autenticação e melhorias

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,15 +33,11 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Login(string login, string senha)
         {
-            if(login != "admin" || senha != "123")
-            {
-                ViewData["Erro"] = "Senha inválida";
-                return View();
-            }
-            else
-            {
-                HttpContext.Session.SetString("user", "admin");
+            if(Autenticacao.verificaLoginSenha(login,senha,this)){
                 return RedirectToAction("Index");
+            }else{
+                ViewData["Erro"] = "Senha invalida";
+               return View(); 
             }
         }
 
